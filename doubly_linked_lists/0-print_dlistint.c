3f64b055a8ad0f9f80024eee9291a6dd9df90b2b
BradBrown6621 <6621@holbertonstudents.com>
2024-03-16 10:12:09 -0500

    Added betty styling to 0-

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints the data elements of a doubly linked list.
+ *
+ * @h: The doubly linked list to be printed
+ *
+ * Return: The number of data elements in the list.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t listlength = 0;
