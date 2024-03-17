fd5442b4b6af3c3a66a5ab1dc36f56eccd3ed521
BradBrown6621 <6621@holbertonstudents.com>
2024-03-17 09:59:02 -0500

    Added 4- and edited header file (lists.h) accordingly

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees the memory for a doubly-linked list
+ *
+ * @head: The pointer to the head node of the list to be free'd
+ *
+ * Return: NOTHING
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head->next)
+	{
+		free_dlistint(head->next);
+	}
+	free(head);
+}
