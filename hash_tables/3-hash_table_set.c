17d003ce1660a5fcee55280e3b9ddc142da12ead
BradBrown6621 <6621@holbertonstudents.com>
2024-04-07 15:39:56 -0500

    Attempted fix for 3-'s malloc return problem

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -75,7 +75,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht)
 		return (0);
 
-	if (is_collision(ht->array[index], key, value_cpy))
+	if (is_collision(ht->array[index], key, strdup(value)))
 		return (1);
 
 	newNode = malloc(sizeof(hash_node_t));
