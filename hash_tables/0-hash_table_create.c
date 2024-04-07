111449cc1f09e386a62614f261cf237ac1ad7921
BradBrown6621 <6621@holbertonstudents.com>
2024-04-06 23:07:51 -0500

    Betty styling for 0-

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a new hash table
+ *
+ * @size: The size of the array of hash nodes
+ *
+ * Return: A new hash_table (or NULL on failure)
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
