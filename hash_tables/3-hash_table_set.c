63adffef05d0bed25b834ab555f2bf00156cd559
BradBrown6621 <6621@holbertonstudents.com>
2024-04-07 15:23:10 -0500

    Added betty styling

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,10 +1,30 @@
 #include "hash_tables.h"
 
+/**
+ * node_prepend - Prepends nodes in a collision chain
+ *
+ * @node: The node to be added
+ * @ht: The hash table to be altered
+ * @index: The index of the hash table array to be altered
+ *
+ * Return: Nothing
+ */
+
 void node_prepend(hash_node_t *node, hash_table_t *ht, unsigned long int index)
 {
 	node->next = ht->array[index];
 	ht->array[index] = node;
 }
+
+/**
+ * is_collision - Checks whether the index of hash table array has collisions
+ *
+ * @beginning: The first node in the chain at a hash table array index
+ * @key: The key to be checked for
+ * @value: The value that nodes can be updated with
+ *
+ * Return: 0 if no collisions reported, 1 if collisions
+ */
 
 int is_collision(hash_node_t *beginning, const char *key, char *value)
 {
@@ -22,6 +42,31 @@ int is_collision(hash_node_t *beginning, const char *key, char *value)
 	return (0);
 }
 
+/**
+ * hash_table_set - Sets new data into a hash table
+ *
+ * @ht: The hash table to be altered
+ * @key: The key to set new data at
+ * @value: The value associated with key
+ *
+ * Description: This function will do one of 3 things:
+ *
+ * 1) If a node does not exist at the hashed index of the hash table array,
+ * the program mallocs a new node, initializes it's key/value pair, and
+ * inserts the new node into that index.
+ *
+ * 2) If there is a node that exists at that index, then:
+ *
+ * - If any of the nodes' keys match the new key, that node's value is updated
+ *   to the new value.
+ *
+ * - If there are no nodes with matching keys to our new key, then we prepend
+ *   a new node with data to the beginning of that chain at that index of the
+ *   hash table array.
+ *
+ * Return: 0 on failure to create/update nodes, 1 on successful creation/update
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
@@ -30,7 +75,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *key_cpy = NULL;
 
 	if (!ht)
-		return (NULL);
+		return (0);
 
 	if (!value_cpy)
 		return (0);
