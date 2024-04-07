39b3fcd4a29e8983a167973d01c9c4ddd9a79e9e
BradBrown6621 <6621@holbertonstudents.com>
2024-04-07 16:31:28 -0500

    Added betty documentation/styling to 4-

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,9 +1,18 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - Gets a value from a key/value pair in a hash table
+ *
+ * @ht: The hash table to be checked for key/value pair
+ * @key: The key used to find key/value pair
+ *
+ * Return: Value (or NULL on failure)
+ */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *current = NULL;
-	long unsigned int i;
+	unsigned long int i;
 
 	if (!ht || !key)
 	{
