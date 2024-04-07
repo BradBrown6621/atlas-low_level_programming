8d4ad310b6324a96e8f9bbfaecaf0216bb26ebce
BradBrown6621 <6621@holbertonstudents.com>
2024-04-07 10:45:52 -0500

    Betty documentation fixes for 1-

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - Hashes a string to an unsigned long integer
+ *
+ * @str: The string to be hashed
+ *
+ * Return: The hash number to be used for finding the index later in the program
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
