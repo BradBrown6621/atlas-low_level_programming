b2d44b063cda389fff0b7716315bac2a0c261425
BradBrown6621 <6621@holbertonstudents.com>
2024-04-07 23:16:26 -0500

    Fixed betty styling/documentation

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * free_chain - Recursively free's a collision chain in a hash table array
+ *
+ * @current: The current node to be free'd
+ *
+ * Return: Nothing
+ */
+
 void free_chain(hash_node_t *current)
 {
 	if (!current)
@@ -12,9 +20,18 @@ void free_chain(hash_node_t *current)
 	free(current);
 }
 
+/**
+ * hash_table_delete - Free's all the memory for a hash table
+ *
+ * @ht: The hash table to be free'd
+ *
+ * Return: Nothing
+ */
+
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
+
 	if (!ht)
 		return;
 
