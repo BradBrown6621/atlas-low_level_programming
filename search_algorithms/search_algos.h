d1af195c4ddfae10a3b7695eb2b88f9271f0eac1
BradBrown6621 <6621@holbertonstudents.com>
2024-04-15 08:49:01 -0500

    Added README and header (search_algos)

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef USR_SEARCH_ALGOS
+#define USR_SEARCH_ALGOS
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
